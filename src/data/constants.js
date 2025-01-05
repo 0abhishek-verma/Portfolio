01a06c236bbc8cb5ff514ab9d12a9e5cea16d442
Abhishek250509 <av2000546@gmail.com>
2025-01-05 16:43:54 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/0abhishek-verma",
     resume:
-      "https://drive.google.com/drive/folders/1J8105g3ktOc_CQ0VlaknNWbBUZ_nkPfe",
+      "https://drive.google.com/file/d/1sqgu5ezRChKPtrZMR6bKgmZ_k6Mezpv_/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/0abhishek-verma/",
     twitter: "https://twitter.com/abhishekve78405",
     insta: "https://www.instagram.com/abhishek_verma2501/",
