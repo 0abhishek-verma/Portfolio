55fe3d7f4c6cd4dc24ffd49ad6c8bdd8e9a9a2cc
Abhishek <155211972+0abhishek-verma@users.noreply.github.com>
2025-04-04 21:53:49 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/0abhishek-verma",
     resume:
-      "https://0abhishek-verma.github.io/Portfolio/",
+      "https://drive.google.com/file/d/1uBzKe_iJA4BeirC5xYW_E0E-4DLwNVkU/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/0abhishek-verma/",
     twitter: "https://twitter.com/abhishekve78405",
     insta: "https://www.instagram.com/abhishek_verma2501/",
